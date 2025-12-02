8e9c0f599b43af766bd72894903a30580e67e028
Eslam Amin <eslam.amin.610@gmail.com>
2025-12-02 16:37:41 +0200

    Updating along with the course's instructor

--- types/structs/fields/begin/main.go
+++ types/structs/fields/begin/main.go
@@ -1,13 +1,22 @@
 // types/structs/fields/begin/main.go
 package main
 
+import "fmt"
+
 // define a struct type for author
 //
+type author struct {
+	firstName, lastName string
+}
 
 func main() {
 	// intialize author
 	//
+	a := author{
+		"Eslam",
+		"Amin",
+	}
 
-	// print the author
-	// fmt.Printf("%#v\n", a)
+	// print the author, type, and value
+	fmt.Printf("%#v\n", a)
 }
--- types/structs/pointers/begin/main.go
+++ types/structs/pointers/begin/main.go
@@ -1,6 +1,8 @@
 // types/structs/pointers/begin/main.go
 package main
 
+import "fmt"
+
 type author struct {
 	first string
 	last  string
@@ -13,17 +15,20 @@ func (a author) fullName() string {
 
 // changeName changes the first and last name of the author
 //
-
+func (a *author) changeNames(firstName, lastName string) {
+	a.first = firstName
+	a.last = lastName
+}
 func main() {
-	// a := author{
-	// 	first: "Mar1",
-	// 	last:  "Twain",
-	// }
+	a := author{
+		first: "Marc",
+		last:  "Twain",
+	}
 
-	// fmt.Println(a.fullName())
+	fmt.Println(a.fullName())
 
 	// call changeName to update name of author
 	//
-
-	// fmt.Println(a.fullName())
+	a.changeNames("Eslam", "Amin")
+	fmt.Println(a.fullName())
 }
