8c30b0a2e1854f86387403efb5d0dba304e0645c
Eslam Amin <eslam.amin.610@gmail.com>
2025-12-02 23:31:52 +0200

    Created out first go routrine with channel

--- concurrency/channel-basics/begin/main.go
+++ concurrency/channel-basics/begin/main.go
@@ -1,26 +1,27 @@
 // concurrency/channels/begin/main.go
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 // sum calculates and prints the sum of numbers
-func sum(nums []int) {
+func sum(nums []int, ch chan<- int) {
 	sum := 0
 	for _, v := range nums {
 		sum += v
 	}
-	fmt.Println("Result:", sum)
+	// fmt.Println("Result:", sum)
+	ch <- sum
 }
 
 func main() {
 	nums := []int{1, 2, 3, 4, 5}
-
+	ch := make(chan int)
+	fmt.Println(nums)
 	// invoke the sum function as a goroutine
-	go sum(nums)
+	go sum(nums, ch)
 
+	result := <-ch
+	fmt.Println("Result:", result)
 	// force main thread to sleep
-	time.Sleep(100 * time.Millisecond)
+	// time.Sleep(100 * time.Millisecond)
 }
