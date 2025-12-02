08b1dd5febbe7d11b7a4ab5314cce9329d8c4f0a
Eslam Amin <eslam.amin.610@gmail.com>
2025-12-02 19:42:59 +0200

    Created a func with type assertions

--- interfaces/type-assertions/begin/main.go
+++ interfaces/type-assertions/begin/main.go
@@ -1,8 +1,17 @@
 // interfaces/type-assertions/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// perform a type assertion
-
+	var i any = "hello"
+	// str := i.(string)
+	fmt.Println(i.(string))
 	// perform a type assertion and handle the error
+	str, ok := i.(string)
+	if !ok {
+		fmt.Printf("%v is not a string\n", i)
+		fmt.Println(str)
+	}
 }
